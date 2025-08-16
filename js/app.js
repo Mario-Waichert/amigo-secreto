8cfde457832533bb819bf721a282d84a47025b7c
Mario Waichert <waichert.mario@gmail.com>
2025-08-16 12:51:20 +0100

    criação do array amigos e função embaralha

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
+    amigos.push(amigo.value);
     if (lista.textContent == ''){
         lista.textContent = amigo.value;
     } else {
         lista.textContent = lista.textContent + ', ' + amigo.value;
     }
     amigo.value = '';
+}
+
+function sortear() {
+    embaralha(amigos);
+}
+
+function embaralha(lista) {
+
+    let indice = lista.length
+    
+    while(indice) {
+        // atenção para o pós-incremento indice-- 
+        const indiceAleatorio = Math.floor(Math.random() * indice--);
+        [lista[indice], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice]];
+    }
 }

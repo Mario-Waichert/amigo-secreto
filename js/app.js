3510fe0b016644c4c9f34ccc0fe1df0db3741217
Mario Waichert <waichert.mario@gmail.com>
2025-09-13 11:21:05 +0100

    permitir ao usuário clicar em um amigo na lista e removê-lo. Atualizar a visualização da lista após a remoção.

--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@ function adicionar() {
         lista.textContent = lista.textContent + ', ' + amigo.value;
     }
     amigo.value = '';
+
+    atualizarLista();
+    atualizarSorteio();
 }
 
 function sortear() {
@@ -27,6 +30,12 @@ function sortear() {
     }
 }
 
+function excluirAmigo(index) {
+    amigos.splice(index, 1);
+    atualizarLista();
+    atualizarSorteio();
+}
+
 function embaralha(lista) {
 
     let indice = lista.length
@@ -36,6 +45,33 @@ function embaralha(lista) {
         const indiceAleatorio = Math.floor(Math.random() * indice--);
         [lista[indice], lista[indiceAleatorio]] = 
             [lista[indiceAleatorio], lista[indice]];
+    }
+}
+
+function atualizarSorteio() {
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = '';
+}
+
+
+function atualizarLista() {
+    let lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+
+    for (let i = 0; i < amigos.length; i++) {
+        // Cria um elemento de parágrafo para cada amigo
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = amigos[i];
+       
+        // Adiciona um evento de clique para excluir o amigo
+        paragrafo.addEventListener('click', function() {
+            excluirAmigo(i);
+        });
+
+
+        // Adiciona o parágrafo à lista
+        lista.appendChild(paragrafo);
     }
 }
 
